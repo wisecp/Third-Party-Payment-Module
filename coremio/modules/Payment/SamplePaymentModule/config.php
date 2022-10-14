8da089491f10f632f5af899f89700cbe0836d003
WISECP <info@wisecp.com>
2022-10-14 14:13:55 +0300

    1.0

---
+++ coremio/modules/Payment/SamplePaymentModule/config.php
@@ -0,0 +1,13 @@
+<?php 
+    return [
+        'meta'     => [
+            'name'    => 'Third Party Payment',
+            'version' => '1.0',
+        ],
+        'settings' => [
+            'commission_rate'  => 0,
+            'force_convert_to' => 0,
+            'accepted_countries'    => [],
+            'unaccepted_countries'  => [],
+        ],
+    ];
